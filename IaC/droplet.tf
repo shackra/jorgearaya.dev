7e2b047c499716b739caaa54d975eadce379d608
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-06-23 17:27:16 -0600

    fix(iac): Increase size of website droplet

--- IaC/droplet.tf
+++ IaC/droplet.tf
@@ -1,11 +1,12 @@
 resource "digitalocean_droplet" "website" {
-  name     = "jorgearaya.dev"
-  image    = data.digitalocean_image.default_nixos.id
-  size     = var.sizes["medium"]
-  region   = "nyc1"
-  tags     = ["nixos"]
-  vpc_uuid = digitalocean_vpc.default.id
-  backups  = true
+  name        = "jorgearaya.dev"
+  image       = data.digitalocean_image.default_nixos.id
+  size        = var.sizes["medium"]
+  resize_disk = true
+  region      = "nyc1"
+  tags        = ["nixos"]
+  vpc_uuid    = digitalocean_vpc.default.id
+  backups     = true
   backup_policy {  # because this droplet has Nextcloud installed
     plan = "daily" # increase the cost by ~30%
     hour = 8       # 2am Costa Rica time
