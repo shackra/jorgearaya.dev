4448ae298f8a72b4c768a518525ee55903b790a2
Jorge Javier Araya Navarro <jorge@esavara.cr>
2026-06-23 20:39:40 -0600

    fix(iac): Resize back to the original DO slug

--- IaC/vars.tf
+++ IaC/vars.tf
@@ -4,7 +4,7 @@ variable "sizes" {
   default = {
     micro    = "s-1vcpu-512mb-10gb" # ~4 USD/mes
     small    = "s-1vcpu-1gb"        # ~6 USD/mes
-    medium   = "s-1vcpu-2gb-amd"    # ~14 USD/mes
+    medium   = "s-1vcpu-2gb"        # ~14 USD/mes
     medium_x = "s-2vcpu-2gb"        # ~18 USD/mes
   }
 }
